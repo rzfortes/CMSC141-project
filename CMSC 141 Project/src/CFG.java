7cfdad68c5c8fd29571f1156eeb6f8f9e249f514
rzfortes <rzfortes@up.edu.ph>
2017-05-19 16:01:30 +0800

    update

--- CMSC 141 Project/src/CFG.java
+++ CMSC 141 Project/src/CFG.java
@@ -77,7 +77,8 @@ public class CFG {
 	public static void main(String[] args) {
 		
 		CFG cfg1 = new CFG();
-		//		S -> VRB + SP + O + PP
+		
+		// for now, initialize productions
 		cfg1.add_prod("NN si NN", "S");
 		cfg1.add_prod("NN ang NN", "S");
 		cfg1.add_prod("Ang NN NN", "S");
@@ -97,8 +98,8 @@ public class CFG {
 		cfg1.add_prod("VRB SP", "S");
 		cfg1.add_prod("ADV NN", "S");
 		cfg1.add_prod("VRB SP AP", "S");
-		cfg1.add_prod("NN|NNP|NNI NN|NNI mga NN|si NN", "SP");
-		cfg1.add_prod("mga NN", "SP");
+		cfg1.add_prod("NN|NNP|NNI NN|NNI PZ NN|si NN", "SP");
+		cfg1.add_prod("PZ NN", "SP");
 		cfg1.add_prod("og ADJ|ADV", "AP");
 		cfg1.add_prod("PREP sa NN", "PP");
 		cfg1.add_prod("PREP NN", "PP");
@@ -107,18 +108,17 @@ public class CFG {
 		cfg1.add_prod("FOR NN", "PP");
 		cfg1.add_prod("FOR ni NN", "PP");
 		cfg1.add_prod("FOR kang NN", "PP");
-		cfg1.add_prod("gikan sa NN", "PP");
-		cfg1.add_prod("gikan NN", "PP");
-		cfg1.add_prod("gikan ni NN", "PP");
-		cfg1.add_prod("gikan kang NN", "PP");
+		cfg1.add_prod("FROM sa NN", "PP");
+		cfg1.add_prod("FROM NN", "PP");
+		cfg1.add_prod("FROM ni NN", "PP");
+		cfg1.add_prod("FROM kang NN", "PP");
 		cfg1.add_prod("og NN", "C");
 		cfg1.add_prod("og NN", "O");
 		cfg1.add_prod("og NN", "DO");
 		cfg1.add_prod("sa NN", "IO");
 		cfg1.add_prod("gikan NNP", "OC");
 		
-		
-		// read corpus.txt
+		// read corpus.txt, contains the terminals for each non-terminals
 		try (BufferedReader br = new BufferedReader(new FileReader("corpus.txt"))){
 			
 			String currentLine = "";
